3ef802a866d2f4a0ff9a0d7343754c0ae9443a48
Mohamed Elsayed <jascriptor@gmail.com>
2025-05-21 16:15:28 +0300

    initial commit

---
+++ src/app/(dash-layout)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+type Props = {
+    children: Readonly<React.ReactNode>
+}
+
+export default function DashLayout({ children }: Props) {
+  return (
+    <>
+        <div className="dash-layout">
+            {children}
+        </div>
+    </>
+  )
+}
---
+++ src/components/layout/DashNavbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DashNavbar() {
+  return (
+    <div>DashNavbar</div>
+  )
+}
