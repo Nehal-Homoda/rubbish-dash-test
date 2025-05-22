6b22e7f3c448c2c08591b26a5ee043ac26bae0e2
yara momen asal <yaraassal80@gmail.com>
2025-05-22 17:34:29 +0300

    feat : Add sidebar items

--- src/components/layout/DashSidebar.tsx
+++ src/components/layout/DashSidebar.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import logo from "@/assets/images/logo.png";
 export default function DashSidebar() {
-  const ListItems=[
-    {icon:'',name:'الصفحة الرئيسية'},
-    {icon:'',name:'الخدمات'},
-    {icon:'',name:'المناطق'},
-    {icon:'',name:'المستخدمين'},
-    {icon:'',name:'الباقات'},
-    {icon:'',name:'جامعي القمامة'},
-    {icon:'',name:'الزيارات'},
-   
-  ]
+  const ListItems = [
+    { icon: "", name: "الصفحة الرئيسية", path: "" },
+    { icon: "", name: "الخدمات", path: "" },
+    { icon: "", name: "المناطق", path: "" },
+    { icon: "", name: "المستخدمين", path: "" },
+    { icon: "", name: "الباقات", path: "" },
+    { icon: "", name: "جامعي القمامة", path: "" },
+    { icon: "", name: "الزيارات", path: "" },
+    { icon: "", name: "المدفوعات", path: "" },
+    { icon: "", name: "الاشعارات", path: "" },
+    { icon: "", name: "الارشادات", path: "" },
+    { icon: "", name: "اللافتات", path: "" },
+    { icon: "", name: "الدعم", path: "" },
+    { icon: "", name: "الاعدادات", path: "" },
+  ];
   return (
-  <div className='bg-surface text-white fixed top-0 right-0 h-full'>
-  <p>DashSidebar</p>
-
-  </div>
-  )
+    <div className="bg-surface text-white fixed top-0 right-0 h-full">
+      <div className="uppercase flex items-center">
+        rubbish
+        <div className="size-8 relative bg-white rounded-full flex justify-center items-center">
+          <Image src={logo} alt="Logo" className="size-7 object-contain" />
+        </div>
+      </div>
+      {ListItems.map((item, index) => (
+        <div key={index} className="capitalize">
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
 }
