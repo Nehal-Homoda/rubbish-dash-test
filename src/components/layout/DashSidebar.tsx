62b2717e3ec82e66c2ce173fadd8882c8ef6d4a4
yara momen asal <yaraassal80@gmail.com>
2025-05-22 16:32:58 +0300

    update

--- src/components/layout/DashSidebar.tsx
+++ src/components/layout/DashSidebar.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function DashSidebar() {
+  const ListItems=[
+    {icon:'',name:'الصفحة الرئيسية'},
+    {icon:'',name:'الخدمات'},
+    {icon:'',name:'المناطق'},
+    {icon:'',name:'المستخدمين'},
+    {icon:'',name:'الباقات'},
+    {icon:'',name:'جامعي القمامة'},
+    {icon:'',name:'الزيارات'},
+   
+  ]
   return (
-    <div>DashSidebar</div>
+  <div className='bg-surface text-white fixed top-0 right-0 h-full'>
+  <p>DashSidebar</p>
+
+  </div>
   )
 }
