53d0c1b8573d290f3468db78bd119f198502201d
yara momen asal <yaraassal80@gmail.com>
2025-05-27 03:05:56 +0300

    refactor(text-field): adjust props structure

--- src/components/ui/form/TextFieldYara.tsx
+++ src/components/ui/form/TextFieldYara.tsx
@@ -14,27 +14,14 @@ interface InputProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   errorMessage?: string;
 }
-export default function Input({
-  label,
-  type,
-  placeholder,
-  prependIcon,
-  appendIcon,
-  iconType,
-  closedEye,
-  openedEye,
-  required,
-  value,
-  errorMessage,
-  onChange,
-}: InputProps) {
-  const [inputType, setInputType] = useState(type);
+export default function TextField(props: InputProps) {
+  const [inputType, setInputType] = useState(props.type);
   const [showPassword, setshowPassword] = useState(false);
   const handleShowPassword = () => {
     setshowPassword((prev) => !prev);
     inputType === "password" ? setInputType("text") : setInputType("password");
   };
-  if (type === "password" && (!closedEye || !openedEye)) {
+  if (props.type === "password" && (!props.closedEye || !props.openedEye)) {
     console.error(
       "For password input type, both 'closedEye' and 'openedEye' icons must be provided."
     );
@@ -49,61 +36,67 @@ export default function Input({
             htmlFor=""
             className="absolute bg-background -top-4 capitalize text-base text-foreground px-2"
           >
-            {label}{" "}
-            {required && <span className="text-red-600 text-lg">*</span>}
+            {props.label}{" "}
+            {props.required && <span className="text-red-600 text-lg">*</span>}
           </label>
           <div className="flex items-center justify-between gap-2">
             <div className="prepend-icon text-foreground/65 text-xl">
-              {iconType === "mdi" && (
-                <span className={`mdi ${prependIcon} `}></span>
+              {props.iconType === "mdi" && (
+                <span className={`mdi ${props.prependIcon} `}></span>
               )}
-              {iconType === "fa" && <i className={`fa ${prependIcon} `}></i>}
+              {props.iconType === "fa" && (
+                <i className={`fa ${props.prependIcon} `}></i>
+              )}
             </div>
             <input
               type={inputType}
               className="w-full bg-background border-none outline-none text-sm placeholder:text-foreground/65"
-              placeholder={placeholder}
-              value={value}
-              onChange={onChange}
+              placeholder={props.placeholder}
+              value={props.value}
+              onChange={props.onChange}
             />
             <div className="append-icon text-foreground/65 text-xl">
-              {iconType === "mdi" && (
+              {props.iconType === "mdi" && (
                 <>
-                  {type !== "password" && appendIcon && (
-                    <span className={`mdi ${appendIcon}`}></span>
+                  {props.type !== "password" && props.appendIcon && (
+                    <span className={`mdi ${props.appendIcon}`}></span>
                   )}
 
-                  {type === "password" && closedEye && openedEye && (
-                    <span
-                      className={`mdi ${
-                        showPassword ? openedEye : closedEye
-                      }  cursor-pointer`}
-                      onClick={handleShowPassword}
-                    ></span>
-                  )}
+                  {props.type === "password" &&
+                    props.closedEye &&
+                    props.openedEye && (
+                      <span
+                        className={`mdi ${
+                          showPassword ? props.openedEye : props.closedEye
+                        }  cursor-pointer`}
+                        onClick={handleShowPassword}
+                      ></span>
+                    )}
                 </>
               )}
-              {iconType === "fa" && (
+              {props.iconType === "fa" && (
                 <>
-                  {type !== "password" && appendIcon && (
-                    <i className={`fa ${appendIcon}`}></i>
+                  {props.type !== "password" && props.appendIcon && (
+                    <i className={`fa ${props.appendIcon}`}></i>
                   )}
 
-                  {type === "password" && closedEye && openedEye && (
-                    <i
-                      className={`fa ${
-                        showPassword ? openedEye : closedEye
-                      }  cursor-pointer`}
-                      onClick={handleShowPassword}
-                    ></i>
-                  )}
+                  {props.type === "password" &&
+                    props.closedEye &&
+                    props.openedEye && (
+                      <i
+                        className={`fa ${
+                          showPassword ? props.openedEye : props.closedEye
+                        }  cursor-pointer`}
+                        onClick={handleShowPassword}
+                      ></i>
+                    )}
                 </>
               )}
             </div>
           </div>
         </div>
-        {errorMessage && (
-          <p className="err-msg text-red-600 text-sm">{errorMessage}</p>
+        {props.errorMessage && (
+          <p className="err-msg text-red-600 text-sm">{props.errorMessage}</p>
         )}
       </div>
     </>
