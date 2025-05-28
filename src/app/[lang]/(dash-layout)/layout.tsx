9a5f931f79e7cb4797f8b5fcf09b2e42da4a4dde
yara momen asal <yaraassal80@gmail.com>
2025-05-28 16:09:28 +0300

    refactor: pass lang and dictionary to  Card component

--- src/app/[lang]/(dash-layout)/layout.tsx
+++ src/app/[lang]/(dash-layout)/layout.tsx
@@ -2,7 +2,6 @@ import { getDictionary } from "../../dictionaries";
 import DashSidebar from "@/components/layout/DashSidebar";
 import UICard from "@/components/ui/UICardYara";
 import DashNavbar from "@/components/layout/DashNavbar";
-import Input from "@/components/ui/form/TextFieldYara";
 
 export default async function RootLayout({children,params,}: Readonly<{children: React.ReactNode;params: Promise<{ lang: "en" | "ar" }>;}>) {
   const { lang } = await params;
@@ -19,7 +18,8 @@ export default async function RootLayout({children,params,}: Readonly<{children:
         <div className="md:ps-[16.9rem] w-[97%] mx-auto">
           <DashNavbar lang={lang} dict={dict} />
           {children}
-          <UICard title={"الخريطة"}>example</UICard>
+          <UICard title={"الخريطة"} lang={lang} dict={dict}>example</UICard>
+
         </div>
       </div>
     </>
