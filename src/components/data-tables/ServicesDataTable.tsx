b6ca870b4445259ab3749cb0f1236d5740dbe553
Your Name <na835126@gmail.com>
2025-06-10 13:50:18 +0300

    feat: add edit service dialoge

--- src/components/data-tables/ServicesDataTable.tsx
+++ src/components/data-tables/ServicesDataTable.tsx
@@ -5,10 +5,17 @@ import { Column } from "primereact/column";
 import alert from "@/assets/images/alert.png";
 import { Tag } from "primereact/tag";
 import BaseModal from "@/components/ui/BaseModal";
+import { useLangAndDictionary } from "@/utils/lang";
+import TextFieldNada from "../ui/form/TextFieldNada";
+import BaseDropdown from "../ui/form/Dropdown";
+import FileInput from "../ui/form/FileInput";
 
 export default function ServicesDataTable() {
   const [selectedProducts, setSelectedProducts] = useState(null);
   const [rowClick, setRowClick] = useState(true);
+  const [arabicName, setArabicName] = useState("");
+  const [englishName, setEnglishName] = useState("");
+  const [status, setStatus] = useState("");
   const [data, setData] = useState<any[]>([
     {
       id: 1,
@@ -39,6 +46,7 @@ export default function ServicesDataTable() {
       subscription: "3 مشترك",
     },
   ]);
+  const {lang,dict} = useLangAndDictionary()
   const statusBodyTemplate = (data) => {
     return <Tag value={data.status} severity={getSeverity(data)}></Tag>;
   };
@@ -74,16 +82,61 @@ export default function ServicesDataTable() {
     return (
       <div className="flex items-center justify-center gap-2">
         <BaseModal
-          title={"تعديل عنصر"}
-          actionBtn={"حفظ"}
+          title={dict.edit_service || "تعديل خدمة"}
+          actionBtn={dict.save || "حفظ"}
           openBtnIcon={"mdi mdi-square-edit-outline"}
           iconType="mdi"
           style="text-surface-light-200 bg-surface-light-800/50 px-2 py-1 rounded-lg"
         >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-            vero!
-          </p>
+          <div className="mt-10 mb-9">
+          <FileInput state="edit" />
+          </div>
+          <TextFieldNada
+            value={arabicName}
+            label={"اسم الخدمة (عربي)"}
+            prependIcon={"mdi mdi-package-variant-closed"}
+            iconType="mdi"
+            placeholder={dict.service_name || "اسم الخدمة"}
+            name="service-name"
+            type="text"
+            required={true}
+            handleChange={(e) => {
+              setArabicName(e.target.value);
+            }}
+          />
+          <div className="my-9">
+            <TextFieldNada
+              value={englishName}
+              label={"اسم الخدمة (انجليزي)"}
+              prependIcon={"mdi mdi-package-variant-closed"}
+              iconType="mdi"
+              placeholder={dict.service_name || "اسم الخدمة"}
+              name="service-name"
+              type="text"
+              required={true}
+              handleChange={(e) => {
+                setEnglishName(e.target.value);
+              }}
+            />
+          </div>
+          <BaseDropdown
+            style="relative text-foreground px-3 py-1.5 border border-surface-light-700 rounded-2xl mt-6"
+            value={status}
+            onChange={({ value }: { value: string }) => {
+              setStatus(value);
+            }}
+            label="الحالة"
+            placeholder="اختر الحالة"
+            name="status"
+            required={true}
+            iconType="mdi"
+            prependIcon="mdi mdi-account-outline"
+            options={[
+              { value: "active", label: "مفعل" },
+              { value: "not-active", label: `غير مفعل` },
+              { value: "pending", label: "معلق" },
+            ]}
+          />
         </BaseModal>
         <BaseModal
           title={"حذف عنصر"}
