0cbcd5b9a421f39dd8eb234d084b7af9fd4efa5f
yara momen asal <yaraassal80@gmail.com>
2025-06-10 13:10:29 +0300

    feat: add ability to customize file input based on state

--- src/components/ui/form/FileInput.tsx
+++ src/components/ui/form/FileInput.tsx
@@ -5,35 +5,42 @@ import { useState, useEffect } from "react";
 
 interface FileInputProps {
   errorMessage?: string;
-  title: string;
+  title?: string;
   onFileChange: (file: File | null) => void;
   fileUrl?: string;
+  state: "edit" | "add" | "addToTable";
 }
 
-export default function FileInput(props: FileInputProps) {
+export default function FileInput({
+  errorMessage,
+  title,
+  onFileChange,
+  fileUrl,
+  state = "add",
+}: FileInputProps) {
   const [fileName, setFileName] = useState("");
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const [fileType, setFileType] = useState<string | null>(null);
 
   useEffect(() => {
-    if (!imageUrl && props.fileUrl) {
-      if (props.fileUrl.endsWith(".pdf")) {
+    if (!imageUrl && fileUrl) {
+      if (fileUrl.endsWith(".pdf")) {
         setFileType("application/pdf");
-        const parts = props.fileUrl.split("/");
+        const parts = fileUrl.split("/");
         setFileName(parts[parts.length - 1]);
         setImageUrl(null);
-      } else if (props.fileUrl.match(/\.(jpeg|jpg|png|gif|webp)$/i)) {
+      } else if (fileUrl.match(/\.(jpeg|jpg|png|gif|webp)$/i)) {
         setFileType("image/*");
-        setImageUrl(props.fileUrl);
-        const parts = props.fileUrl.split("/");
+        setImageUrl(fileUrl);
+        const parts = fileUrl.split("/");
         setFileName(parts[parts.length - 1]);
       }
     }
-  }, [props.fileUrl, imageUrl]);
+  }, [fileUrl, imageUrl]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
-    props.onFileChange(file);
+    onFileChange(file);
     if (file) {
       setFileType(file.type);
       setFileName(file.name);
@@ -56,8 +63,14 @@ export default function FileInput(props: FileInputProps) {
 
   return (
     <>
-      <p className="text-foreground mb-5">{props.title}</p>
-      <div className="relative overflow-hidden  w-fit">
+      {title && <p className="text-foreground mb-5">{title}</p>}
+      <div
+        className={`relative  ${
+          state === "add"
+            ? "border-2 border-dashed rounded-lg "
+            : " rounded-2xl shadow-xl"
+        } w-fit`}
+      >
         <input
           type="file"
           className="size-full absolute top-0 left-0 right-0 z-50 opacity-0 cursor-pointer "
@@ -65,7 +78,12 @@ export default function FileInput(props: FileInputProps) {
           accept="image/*,application/pdf"
           title={fileName}
         />
-        <div className="relative border-2 border-dashed rounded-lg flex items-center justify-center w-80 h-24 gap-1">
+
+        <div
+          className={` relative  flex items-center justify-center ${
+            state === "add" ? " w-80 h-24" : " w-32 h-20"
+          } gap-1 `}
+        >
           {fileType?.startsWith("image/") && imageUrl ? (
             <Image
               src={imageUrl}
@@ -76,32 +94,46 @@ export default function FileInput(props: FileInputProps) {
             />
           ) : fileType === "application/pdf" && fileName ? (
             <p className="text-foreground/50 text-sm text-center">{fileName}</p>
-          ) : (
+          ) : state === "add" ? (
             <>
-              <span className="mdi mdi-image-outline text-foreground/50 text-lg"></span>
+              <i className="fa-regular fa-image text-foreground/50  text-lg"></i>
               <span className="text-foreground/50 text-sm">
                 ارفاق صورة التحويل
               </span>
             </>
+          ) : (
+            <>
+              <i className="fa-regular fa-image text-foreground/50 text-3xl"></i>
+            </>
           )}
         </div>
-
-      
-        {props.errorMessage && (
-          <p className="err-msg text-red-600 text-sm">{props.errorMessage}</p>
+        {state === "edit" && (
+          <div className="">
+            <i className="fa-solid fa-xmark cursor-pointer absolute -top-2 -right-2 text-surface bg-surface-light-800 p-2 text-sm rounded-full"></i>
+            <i className="fa-solid fa-pen cursor-pointer absolute -bottom-2 -left-2 text-surface bg-surface-light-800  p-2 text-sm rounded-full"></i>
+          </div>
         )}
-      </div>  {fileType !== "application/pdf" && fileName && (
-          <p className="text-foreground/70 mt-2">{fileName}</p>
+        {state === "addToTable" && (
+          <div className="">
+            <i className="fa-solid fa-plus cursor-pointer absolute -bottom-2 -left-2 text-surface bg-surface-light-800  p-2 text-sm rounded-full"></i>
+          </div>
         )}
-        {fileType === "application/pdf" && fileName && props.fileUrl && (
-          <Link
-            href={props.fileUrl}
-            target="_blank"
-            className="text-foreground mt-2 underline hover:text-foreground/70"
-          >
-            {fileName}
-          </Link>
+        {errorMessage && (
+          <p className="err-msg text-red-600 text-sm">{errorMessage}</p>
         )}
+      </div>
+      {fileType !== "application/pdf" && fileName && (
+        <p className="text-foreground/70 mt-2">{fileName}</p>
+      )}
+      {fileType === "application/pdf" && fileName && fileUrl && (
+        <Link
+          href={fileUrl}
+          target="_blank"
+          className="text-foreground mt-2 underline hover:text-foreground/70"
+        >
+          {fileName}
+        </Link>
+      )}
     </>
   );
 }
