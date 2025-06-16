a1a59fc8379e94a9bd4677a8874473a3bf4c26b0
unknown <nehal>
2025-06-16 18:13:10 +0300

    update

--- src/components/data-tables/customDataTable.tsx
+++ src/components/data-tables/customDataTable.tsx
@@ -9,6 +9,8 @@ export default function
     <>
       <div className='container py-20'>
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
+          
+          
           <div>
             <form className="max-w-md mx-auto">
               <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -21,6 +23,8 @@ export default function
                 <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
               </div>
             </form>
+
+            
 
 
           </div>
