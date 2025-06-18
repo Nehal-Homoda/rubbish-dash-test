c83f4cfbbb3773161ccac49950fab24858234eaa
Mohamed Elsayed <jascriptor@gmail.com>
2025-06-18 21:52:30 +0300

    some retoutches

--- src/app/[lang]/(dash-layout)/rubbush-collectors/_StoredPage.tsx
+++ src/app/[lang]/(dash-layout)/rubbush-collectors/_StoredPage.tsx
@@ -174,9 +174,9 @@ export default function rubbush_collectors() {
   }, []);
   return (
     <>
-      <div className="py-20">
-
-
+      {" "}
+      <div>
+        rubbush_collectors
         <CustomDataTable
           handleSort={sortList}
           handleAllCheck={takeCheckValue}
@@ -190,6 +190,22 @@ export default function rubbush_collectors() {
                     key={index}
                     className=" bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-100 hover:bg-gray-50 dark:hover:bg-gray-600 "
                   >
+                    <td className="py-2 px-4">
+                      <div className="flex items-center">
+                        <input
+                          checked={checkBoxValue}
+                          id="checkbox-table-search-1"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          htmlFor="checkbox-table-search-1"
+                          className="sr-only"
+                        >
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
                     <td className="py-2 px-4">{item.id}</td>
                     <td className="py-2 px-4">{item.name}</td>
                     <td className="py-2 px-4">{item.phone}</td>
